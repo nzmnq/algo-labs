6357c3671c06809506cd7c296b791c3c2e132101
Nazar <nazari.doskochynskyi@gmail.com>
2026-02-21 11:35:18 +0200

    lab2 file added

---
+++ lab2.py
@@ -0,0 +1,21 @@
+def hamster(S = int, C = int, hamsters = list[list[int]] ) -> int:
+    low = 0
+    high = C
+    better_count = 0
+
+    while low <- high:
+        mid = (low + high) // 2
+        
+        if mid == 0:
+            low = mid + 1
+            continue
+        cost = [h + g * (mid - 1) for h, g in hamster]
+        cost.sort()
+        total_cost = sum(cost[:mid])
+
+        if total_cost <= sum:
+            better_count = mid
+            low = mid + 1
+        else:
+            high = mid -1
+    return better_count
