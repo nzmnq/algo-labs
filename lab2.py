6615a2ee119c78e0db76e09f11339b83d2a1a5c4
Nazar <nazari.doskochynskyi@gmail.com>
2026-02-23 09:28:07 +0200

    fixed

--- lab2.py
+++ lab2.py
@@ -3,21 +3,18 @@ def max_hamster(S: int, C: int, hamsters: list[list[int]] ):
         return 0
     low = 0
     high = C
-    better_count = 0
+    max_count = 0
 
     while low <= high:
         mid = (low + high) // 2
-        
-        if mid == 0:
-            low = mid + 1
-            continue
+
         cost = [h + g * (mid - 1) for h, g in hamsters]
         cost.sort()
         total_cost = sum(cost[:mid])
 
         if total_cost <= S:
-            better_count = mid
+            max_count = mid
             low = mid + 1
         else:
             high = mid -1
-    return better_count
+    return max_count
